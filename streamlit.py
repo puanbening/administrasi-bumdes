1cb87169bff28cd11d0f76840b8d69fb412b8a5c
Puan Bening Pastika <146619358+puanbening@users.noreply.github.com>
2025-11-21 13:24:53 +0700

    update tanggal streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -190,7 +190,7 @@ with tab1:
 
     # Tombol tambah baris untuk Jurnal Umum
     if st.button("➕ Tambah Baris Jurnal", key="tambah_jurnal"):
-        new_row = pd.DataFrame([{"Tanggal": "", "Keterangan": "", "Akun": "", "Debit (Rp)": 0, "Kredit (Rp)": 0}])
+        new_row = pd.DataFrame([{"Tanggal": None, "Keterangan": "", "Akun": "", "Debit (Rp)": 0, "Kredit (Rp)": 0}])
         st.session_state.data = pd.concat([st.session_state.data, new_row], ignore_index=True)
         st.rerun()
 
@@ -231,9 +231,10 @@ with tab1:
 
     new_df = pd.DataFrame(grid_response["data"])
     new_df["Tanggal"] = pd.to_datetime(
-    new_df["Tanggal"].replace({"": None}),
+        new_df["Tanggal"],
         errors="coerce"
     ).dt.date
+
     if not new_df.equals(st.session_state.data):
         st.session_state.data = new_df.copy()
 
